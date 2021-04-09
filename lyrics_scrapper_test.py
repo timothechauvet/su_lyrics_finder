d9636c73d0abf6f501fb6a0673fb1462624e4b67
timothecht <timothe.chauvet@efrei.net>
2021-04-09 17:51:43 +0200

    Working scrapper

--- lyrics_scrapper_test.py
+++ lyrics_scrapper_test.py
@@ -4,11 +4,13 @@ import pandas as pd
 import numpy as np
 import string
 import re
+import pprint
 
 def json_lyrics_creation(filename = "output_songs.json",
                          artist_name = "Steven Universe", 
                          max_songs = 15,
-                         token = "dyuHwfM-LUID2D_Ia9vEZneBJLidAlHgasnNyzdBdYkQb7Qrx37E0aIVB51qNuSw"):
+                         token = "znGu5Y5LvZ2pMT3XxHmU-jPMC3bTqpysfTiKOzew49nSiyqDbgSBnfNGUh1_AYRH"):
+    pp = pprint.PrettyPrinter(width=200, indent=1)
     # Connect to Genius API                         
     genius = lyricsgenius.Genius(token)
 
@@ -23,21 +25,22 @@ def json_lyrics_creation(filename = "output_songs.json",
 
     artist = genius.search_artist(artist_name=artist_name, max_songs=max_songs, sort="title")
     for song in artist.songs:
-        song_lyrics = re.sub("\n", " ", song.lyrics)
         song_name = song.title
-        
-        # Remove null songs and append them to the JSON
-        if (song_lyrics != "") and (song_name != ""):
-            # tmp = {}
-            # tmp["name"] = []
-            # tmp["lyrics"]
-            SUsongs["songs"].append(song_name)
-            SUsongs["lyrics"].append(song_lyrics)
-    
-    df = pd.DataFrame(SUsongs)
-    df.to_json(path_or_buf = "output_songs.json")
 
-def clean_data(raw_song):
+        song_lyrics = clean_lyrics(song.lyrics)
+        
+        # Remove null songs, songs with no singers written and unnamed songs
+        if(len(song_lyrics) > 0 and (song_name != "")):
+            if(len(song_lyrics[0]["singer"]) > 0):
+                song = {}
+                song["title"] = song_name
+                song["lyrics"] = song_lyrics
+                SUsongs["songs"].append(song)
+    
+    with open(filename, 'w') as json_file:
+        json.dump(SUsongs, json_file)
+
+def clean_lyrics(raw_song):
     # Main regex for dispatching singers and lyrics
     regex_singer = r"\S+(?:(?=:))"
     regex_lyrics = r"(?:(?<=:\s))(.+?)(?:(?= \S+:)|(?=$)|(?=\n))"
@@ -47,6 +50,7 @@ def clean_data(raw_song):
     regex_double_space  = r"(\s{2,})"
 
     # Remove unwanted data
+    raw_song = re.sub("\n", " ", raw_song)
     raw_song = re.sub(regex_parenthesis, '', raw_song)
     raw_song = re.sub(regex_double_space, ' ', raw_song)
 
@@ -61,14 +65,13 @@ def clean_data(raw_song):
         line = line.lower()
 
     # Dispatch all lyrics for better Json readability
-    lyrics = {}
-    for i in range(size(result_singer)):
+    lyrics = []
+    for i in range(len(result_singer)):
         line = {}
-        line["singer"]  = result_singer[i]
         line["line"]    = result_line[i]
+        line["singer"]  = result_singer[i]
         lyrics.append(line)
     
     return lyrics
 
-csv_lyrics_creation()
-#df = clean_data()
+json_lyrics_creation()
